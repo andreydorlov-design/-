71f9895212f945f65911b3ecca92a7903bec01c4
andreydorlov-design <andrey.d.orlov@gmail.com>
2026-05-26 20:27:17 +0300

    Add files via upload

---
+++ Скрипт 3.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(r'C:\Users\Андрей\Downloads\flights_NY.csv')
+
+df_clean = df.dropna(subset=['air_time', 'distance'])
+
+df_clean = df_clean[df_clean['air_time'] > 0]
+
+df_clean['speed'] = (df_clean['distance'] / df_clean['air_time']) * 60
+
+df_wn_ua = df_clean[df_clean['carrier'].isin(['WN', 'UA'])].copy()
+
+df_wn_ua.to_csv('flights_speed_WN_UA_clean.csv', index=False)
+
+print("Размер после очистки:", len(df_wn_ua))
+print("Пример данных:\n", df_wn_ua[['carrier', 'distance', 'air_time', 'speed']].head())
